d49421087c37a8298ada9f4d63d60b293145355e
Vivek <vivek.mitra@outlook.com>
2024-03-27 19:12:10 +1100

    deploy script made and hardhat config updated

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,13 @@
+const { network, ethers } = require("hardhat")
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+}
