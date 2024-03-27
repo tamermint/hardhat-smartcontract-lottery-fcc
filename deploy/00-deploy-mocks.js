28c794170a1a5b1b587cc8702e15f6b5b040285b
Vivek <vivek.mitra@outlook.com>
2024-03-27 20:21:23 +1100

    mock deploy contract created and logic added to raffle.sol

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,8 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const developmentChains = require("../helper-hardhat-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -8,5 +11,14 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     if (developmentChains.includes(network.name)) {
         log("local network detected! Deploying mocks..")
+        await deploy("VRFCoordinatorMock", {
+            from: deployer,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+            log: true,
+        })
+        log("Mocks deployed!")
+        log("---------------------------")
     }
 }
+
+module.exports.tags = ["all", "mocks"]
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,8 +1,15 @@
 const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    let vrfCoordinatorV2Address
+
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("vrfCoordinatorV2Mock")
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address()
+    }
 
     const raffle = await deploy("Raffle", {
         from: deployer,
