a5008611c49570c0c4c3ba66568a3396f097dd88
Vivek <vivek.mitra@outlook.com>
2024-03-27 19:30:45 +1100

    mocks added from chainlink contracts

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,12 @@
+const { network } = require("hardhat")
+const developmentChains = require("../helper-hardhat-config")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    if (developmentChains.includes(network.name)) {
+        log("local network detected! Deploying mocks..")
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,9 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL ||
+    "https://eth-sepolia.g.alchemy.com/v2/bKBDD7WLPKM4bWKh52RJoH8AHWwW-7SR"
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
