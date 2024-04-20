3f088c5660dcbd2b03187da13c66321b4ebf94ff
Vivek <vivek.mitra@outlook.com>
2024-04-20 16:50:20 +1000

    removed debug console logging

--- deploy/02-update-front-end.js
+++ deploy/02-update-front-end.js
@@ -1,13 +1,13 @@
 const { ethers, network } = require("hardhat")
 const fs = require("fs")
 const { FRONT_END_ABI_FILE, FRONT_END_CONTRACTS_FILE } = require("../helper-hardhat-config")
-const { networks } = require("../hardhat.config")
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating front end...")
         await updateContractAddresses()
         await updateAbi()
+        console.log("front end written!")
     }
 }
 
@@ -18,15 +18,13 @@ async function updateAbi() {
 
 async function updateContractAddresses() {
     const raffle = await ethers.getContract("Raffle")
-    const chainId = network.config.chainId.toString()
     const contractAddresses = JSON.parse(fs.readFileSync(FRONT_END_CONTRACTS_FILE, "utf8"))
-    console.log(chainId)
-    if (chainId in contractAddresses) {
+    if (network.config.chainId.toString() in contractAddresses) {
         console.log("chainID is in contractAddresses")
-        if (!contractAddresses[chainId].includes(raffle.address)) {
-            contractAddresses[chainId] = raffle.address
+        if (!contractAddresses[network.config.chainId.toString()].includes(raffle.address)) {
+            contractAddresses[network.config.chainId.toString()] = raffle.address
         } else {
-            contractAddresses[chainId] = [raffle.address]
+            contractAddresses[network.config.chainId.toString()] = [raffle.address]
         }
     }
     fs.writeFileSync(FRONT_END_CONTRACTS_FILE, JSON.stringify(contractAddresses))
